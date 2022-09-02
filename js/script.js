1d314adb951bba182959876cd95957b1a0ab0f8a
undanganmanten <agungtprass@gmail.com>
2022-09-03 01:47:53 +0700

    frida v1

--- js/script.js
+++ js/script.js
@@ -110,6 +110,7 @@ var firebaseConfig = {
       updates["/frida/" + key] = task;
       firebase.database().ref().update(updates);
       create_unfinished_task();
+      swal("Pesan telah terkirim", "Silakan cek pesan anda di kolom yang sudah ada", "success");
       document.getElementById("name").value=''; 
       document.getElementById("message").value='';
     }
@@ -157,5 +158,27 @@ function create_unfinished_task(){
       }
   
     });
+  
+  }
+
+  function copyToClipboard(elementId) {
+
+    // Create an auxiliary hidden input
+    var aux = document.createElement("input");
+  
+    // Get the text from the element passed into the input
+    aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+  
+    // Append the aux input to the body
+    document.body.appendChild(aux);
+  
+    // Highlight the content
+    aux.select();
+  
+    // Execute the copy command
+    document.execCommand("copy");
+  
+    // Remove the input from the body
+    document.body.removeChild(aux);
   
   }
